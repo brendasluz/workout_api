edea67d64e93dde6e4ad28d9d16c1430ee9973b0
brendasluz <150370319+brendasluz@users.noreply.github.com>
2025-08-30 17:13:50 -0300

    Update schemas.py
    Inclusao classe AtletaResponse(BaseSchema)

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -27,3 +27,8 @@ class AtletaOut(Atleta, OutMixin):
 class AtletaUpdate(BaseSchema):
     nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
     idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
+
+class AtletaResponse(BaseSchema):
+    nome: str
+    centro_treinamento: CentroTreinamentoAtleta
+    categoria: CategoriaIn
